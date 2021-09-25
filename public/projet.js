b9444cc5573ee2c29c02dba277d0cbc4c6379ef6
Cooleliel <elielcoulibaly39@gmail.com>
2021-09-25 15:02:35 +0000

    mise en ligne du code final du projet intitule portfolio

---
+++ public/projet.js
@@ -0,0 +1,28 @@
+
+let projets =[
+ {
+  name: "projet 1",
+  tags: "#java ",
+  image: "Media.png",
+  },
+ {
+  name: "projet 2",
+  tags: "#static web ",
+  image: "Social.png",
+ },
+ {
+  name: "projet 3",
+  tags: "#c",
+  image: "VivoBook.jpeg",
+ },
+ {
+  name: "projet 4",
+  tags: "#dynamic web ",
+  image: "Huawei.jpeg",
+ },
+ {
+  name: "projet 5",
+  tags: "#flutter ",
+  image: "Beats.png",
+ },
+] 
--- server.js
+++ server.js
@@ -1,13 +1,46 @@
-const express = require('express') ;
+const express = require('express');
 const path = require('path') ;
+const nodemailer = require('nodemailer') ;
+const dotenv = require('dotenv') ;
+
+dotenv.config() ;
 
 let iniPath = path.join(__dirname , 'public') ;
 let app = express() ;
 
 app.use(express.static(iniPath)) ;
+app.use(express.json()) ;
 
 app.get('/' , (req , res) => {
- res.sendFile(path.join(iniPath , 'index.html')) ;
+ res.sendFile(path.join(initialPath , 'index.html')) ;
+})
+
+app.post('/mail ' , (req , res) => {
+ const { firstname , lastname , email , msg} = req.body ;
+
+ const transporter = nodemailer.createTransport({
+  service: 'gmail' ,
+  auth: {
+   user: process.env.EMAIL,
+   pass: process.env.PASSWORD,
+  }
+ })
+
+ const mailOptions = {
+  from: 'Enter the sender email here' ,
+  to: 'Enter the receiver email here' ,
+  subject: 'Postfolio' ,
+  text: `First name: ${firstname} , \nLast name:${lastname} , \nEmail: ${email} \nMessage: ${msg}`
+ }
+
+ transporter.sendMail(mailOptions, (err , result) => {
+  if(err) {
+   console.log(err) ;
+   res.json('Oups! Une erreur est survenue.Reessayez svp.') ;
+  } else{
+   res.json('Merci de m\'avoir contacter . je vous répondrai sous 2 jours ouvrés') ;
+  }
+ })
 })
 
 app.listen('3000' , () => {
